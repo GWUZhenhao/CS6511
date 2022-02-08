1a81e921ee49f728dd94539cc7a3b59fa73e08ef
YingningMa <98433146+YingningMa@users.noreply.github.com>
2022-02-07 21:15:46 -0500

    Add files via upload

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,9 +1,12 @@
 import igraph as ig
 import numpy as np
 import math
-
+from time import time
 
 def construct_graph(containers, target):
+
+    select_list = []
+
     start_state = np.zeros(shape=len(containers))
     start_h_score = calculate_h_score(containers, start_state, target)
 
@@ -17,9 +20,16 @@ def construct_graph(containers, target):
                  h_score = start_h_score,
                  f_score = start_h_score + 0,
                  is_leaf = True)
-
+    i = 0
     while check_graph(g):
-        g = expand_graph(g, containers, target)
+        g = expand_graph(g, containers, target, select_list)
+        #print(g.vs['g_score'][-1])
+        '''
+        if i> 15:
+            break
+        plot(g)
+        i += 1
+        '''
 
     return g
 
@@ -41,8 +51,11 @@ def calculate_h_score(containers, state, target):
     inds_reject = np.where(containers < target)
 
     # Find the nearest value to the target
+
     inds_near = np.argsort(np.absolute(state - target))
+    #print('inds_near:', inds_reject, inds_near, np.absolute(state - target), state)
     ind_nearest = inds_near[0]
+
     for index in inds_near:
         if np.isin(index, inds_reject):
             continue
@@ -53,9 +66,12 @@ def calculate_h_score(containers, state, target):
 
     # calculate the difference between target and the nearest value
     diff_target = np.absolute(value_nearest - target)
-
+    #print('diff_target:', ind_nearest, value_nearest, target, diff_target)
     # calculate the h_score by the diff_target
-    h_score = math.floor(diff_target/np.max(containers[:-1])) + 1
+
+    h_score = math.ceil(diff_target / np.max(containers[:-1]))*2
+    #h_score = math.floor(diff_target/np.max(containers[:-1]))+1
+
 
     return h_score
 
@@ -66,7 +82,7 @@ def check_graph(g):
     for node in nodes:
         if node['goal']:
             return False
-    print(nodes['goal'], nodes['f_score'])
+    #print(nodes['goal'], nodes['f_score'])
     return True
 
 def arreq_in_list(myarr, list_arrays):
@@ -77,12 +93,12 @@ def arreq_in_list(myarr, list_arrays):
             return False
     # return next((True for elem in list_arrays if np.array_equal(elem, myarr)), False)
 
-def expand_graph(graph, containers, target):
+def expand_graph(graph, containers, target, select_list):
     # Only expanding one of leaf nodes
     candidates_to_expand = graph.vs.select(is_leaf=True)
     candidates_to_expand = candidates_to_expand(goal=False)
     node_to_expand = candidates_to_expand.find(f_score=np.min(candidates_to_expand['f_score']))
-
+    #select_list.append(node_to_expand['state'].tolist())
     current_state = node_to_expand['state'] # Store the current state of that node.
 
     #
@@ -92,7 +108,8 @@ def expand_graph(graph, containers, target):
         if water != containers[index] and index != len(current_state) - 1:
             new_state = np.array(current_state)
             new_state[index] = containers[index]
-            graph = add_node(graph, node_to_expand, containers, target, new_state)
+            if new_state.tolist() not in select_list:
+                graph = add_node(graph, node_to_expand, containers, target, new_state)
 
         # If the container has water, we can pour it to other container
         if water != 0:
@@ -103,47 +120,50 @@ def expand_graph(graph, containers, target):
                     if water <= containers[index_target] - current_state[index_target]:
                         new_state[index_target] = current_state[index_target] + water
                         new_state[index] = 0
-                        graph = add_node(graph, node_to_expand, containers, target, new_state)
+                        if new_state.tolist() not in select_list:
+                            select_list.append(new_state.tolist())
+                            graph = add_node(graph, node_to_expand, containers, target, new_state)
                     else:
                         new_state[index_target] = containers[index_target]
                         new_state[index] = water - (containers[index_target] - current_state[index_target])
-                        graph = add_node(graph, node_to_expand, containers, target, new_state)
+                        if new_state.tolist() not in select_list:
+                            select_list.append(new_state.tolist())
+                            graph = add_node(graph, node_to_expand, containers, target, new_state)
 
         # If the container has water, wa can also pour it away.
         if water != 0:
             new_state = np.array(current_state)
             new_state[index] = 0
-            graph = add_node(graph, node_to_expand, containers, target, new_state)
+            if new_state.tolist() not in select_list:
+                select_list.append(new_state.tolist())
+                graph = add_node(graph, node_to_expand, containers, target, new_state)
             
     node_to_expand['is_leaf'] = False
     return graph
 
 def add_node(graph, node_to_expand, containers, target, new_state):
-    if arreq_in_list(new_state, graph.vs['state']):
-        # if we want to make a tree, comment this line
-        # graph.add_edge(node_to_expand, graph.vs.find(state=new_state), weight=1)
-        pass
-    else:
-        new_h_score = calculate_h_score(containers, new_state, target)
-        new_g_score = node_to_expand['g_score'] + 1
-        goal = target in new_state
-        graph.add_vertex(state=new_state,
-                         goal=goal,
-                         g_score=new_g_score,
-                         h_score=new_h_score,
-                         f_score=new_h_score + new_g_score,
-                         is_leaf=True)
-        graph.add_edge(node_to_expand, graph.vs[-1], weight=1)
+
+    new_h_score = calculate_h_score(containers, new_state, target)
+    new_g_score = node_to_expand['g_score'] + 1
+    #print(node_to_expand['state'], node_to_expand['g_score'], node_to_expand['h_score'], node_to_expand['f_score'])
+    goal = target in new_state
+    graph.add_vertex(state=new_state,
+                     goal=goal,
+                     g_score=new_g_score,
+                     h_score=new_h_score,
+                     f_score=new_h_score + new_g_score,
+                     is_leaf=True)
+    graph.add_edge(node_to_expand, graph.vs[-1], weight=1)
     return graph
 
 def plot(graph):
     layout = graph.layout("kk")
     visual_style = {}
-    visual_style["vertex_size"] = 100
+    visual_style["vertex_size"] = 80
     visual_style["vertex_label"] = graph.vs['state']
     visual_style["edge_width"] = graph.es['weight']
     visual_style["layout"] = layout
-    visual_style["bbox"] = (1000, 1000)
+    visual_style["bbox"] = (2000, 2000)
     visual_style["margin"] = 200
     ig.plot(graph, **visual_style)
 
@@ -161,12 +181,23 @@ with open("data.txt", "r") as f:
     input = list(map(int, input))
     print(input)
 
-containers = np.array(input)
-containers = np.append(containers, 999999)
-g = construct_graph(containers, target)
-num_step = calculate_steps(g)
-plot(g)
-print()
+
+t_start=time()
+gcd = np.gcd.reduce(input)
+if target % gcd == 0:
+    containers = np.array(input)
+    containers = np.append(containers, 999999)
+    g = construct_graph(containers, target)
+    num_step = calculate_steps(g)
+    print(num_step)
+else:
+    print(-1)
+
+
+t_end=time()
+t_cost=t_end-t_start
+print(t_cost)
+#plot(g)
 
 
 # # Playground:
